48ff5293879891fc3b3ac27cea151aa3a3d26364
LucasCarmagnola <carmagnola.lucas@gmail.com>
2025-08-26 17:41:08 -0300

    arreglos ejercicio 7

--- DIA-7/ejercicio-7.js
+++ DIA-7/ejercicio-7.js
@@ -1,22 +1,27 @@
 // Pedí al usuario (o definí en el código) una palabra o frase.
 // Recorré el string y contá:
-// cuántas vocales hay (a, e, i, o, u),
+// cuántas vocales hay,
 // cuántas consonantes hay.
 // Mostrá ambos resultados en consola.
 
-
 const verificarPalabra = (palabra) => {
     const vocales = ['a','e','i','o','u']
+    const consonantes = ['b','c','d','f','g','h','j','k','l','m',
+    'n','ñ','p','q','r','s','t','v','w','x','y','z']
     let contadorVocales = 0
-    let consonantes = 0
+    let contadorConsonantes = 0
     let palabraMinuscula = palabra.toLowerCase().replace(/\s/g, '')
     
-    for(letra of palabraMinuscula){
-        vocales.includes(letra) ? contadorVocales ++ : consonantes ++
+    for(let letra of palabraMinuscula){
+        //vocales.includes(letra) ? contadorVocales ++ : contadorConsonantes ++
+        if(vocales.includes(letra)){
+            contadorVocales++
+        }else if(consonantes.includes(letra)){
+            contadorConsonantes++
+        }
     }
 
-    console.log(`El string ingresado contiene ${contadorVocales} vocales y ${consonantes} consonantes`)
-
+    console.log(`El string ingresado contiene ${contadorVocales} vocales y ${contadorConsonantes} consonantes`)
 }
 
 verificarPalabra('Hola Mundo')
